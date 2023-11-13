24b4bee26da3ef437315ca89117790886d5e068d
husniyya05 <husniyyemecnunova@mail.ru>
2023-11-13 19:29:32 +0400

    forst

---
+++ assets/js/form.js
@@ -0,0 +1,12 @@
+const menu = document.querySelector(".menu-home");
+const home = document.querySelector("a-home")
+ function toggleMobile(){
+    if(menu.style.display==="none"){
+        menu.style.display="block";
+    }
+    else{
+        menu.style.display = "none";
+    }
+    
+ }
+ home.addEventListener("click" , menu)
